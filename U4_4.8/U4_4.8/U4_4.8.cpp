1269f5355ba52e432216c4bd4a59540224bad4ba
Plagga <benitopab@gmail.com>
2024-05-03 02:35:06 -0300

    .

---
+++ U4_4.8/U4_4.8/U4_4.8.cpp
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main()
+{
+    int l1, l2, l3;
+
+    printf("Ingrese tres numeros reales positivos: ");
+    scanf_s("%d %d %d", &l1, &l2, &l3);
+
+    if (l1 + l2 > l3 || l1 + l3 > l2 || l3 + l2 > l1) printf("Si cumple con el teorema del triangulo.");
+    else printf("No cumple con el teorema del triangulo.");
+
+    return 0;
+}
